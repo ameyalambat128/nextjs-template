39edb257e9242f2677b8fb621075c3c1d7e71fbd
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2023-01-04 02:16:04 -0500

    fix: fixed title and metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Breze Test</title>
-        <meta name="description" content="Breze Test" />
+        <title>Template</title>
+        <meta name="description" content="Nextjs Template" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="font-bold">Hello World</h1>
+      <h1 className="flex flex-col h-screen items-center justify-center">Hello World</h1>
     </div>
   );
 }
